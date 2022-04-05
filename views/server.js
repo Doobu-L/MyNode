1abf02be20a732a5e20514fe5d44427ecaf85eca
DESKTOP-SA0VI34\Seop <point455@bankq.co.kr>
2022-04-05 14:45:15 +0900

    Routing

--- views/server.js
+++ views/server.js
@@ -2,8 +2,30 @@ var express = require('express');
 var app = express();
 
 app.get('/',function (req,res){
-  res.send('Hello World');
+  res.send('Hello GET');
 })
+
+app.post('/',function (req, res ){
+  console.log("Got a POST request for the hompage");
+  res.send("Hello POST");
+})
+
+app.delete('/',function (req, res ){
+  console.log("Got a DELETE request for the hompage");
+  res.send("Hello DELETE");
+})
+
+app.get('/list_user',function (req, res ){
+  console.log("Got a GET request for /list_user");
+  res.send("Page Listing");
+})
+
+app.get('/ab*cd',function (req, res ){
+  console.log("Got a GET request for /ab*cd");
+  res.send("Page Pattern Match");
+})
+
+
 
 var server = app.listen(8081, function() {
   var host = server.address().address
@@ -11,3 +33,4 @@ var server = app.listen(8081, function() {
 
   console.log("Example app listening at http://%s:%s",host,port);
 })
+
